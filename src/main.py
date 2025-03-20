eddcecc4262056eef02791b113b7275065b3a8dd
generousmouse40 <ryanlisenby@msn.com>
2025-03-19 17:11:59 -0700

    deploy from 'main' and has 'docs' directory

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 import shutil
+import sys
 
 from copystatic import *
 from gencontent import *
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 dir_path_static = os.path.join(base_dir, "../static")
-dir_path_public = os.path.join(base_dir, "../public")
+dir_path_public = os.path.join(base_dir, "../docs")
 template_path = os.path.join(base_dir, "../template.html")
 dir_path_content = os.path.join(base_dir, "../content")
+default_basepath = os.path.join(base_dir, "/")
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -19,6 +25,6 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 main()
